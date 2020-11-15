8e94780a6da934a2fb28631204cc80ec39feaeae
SALMAN-ALTAF <SALMAN-ALTAF@SALMAN-ALTAF-PC>
2020-11-15 16:14:27 +0500

    bugs fixes.

--- src/app/util/regex.js
+++ src/app/util/regex.js
@@ -6,6 +6,7 @@ const getFirstData = (path) => {
     if(!match) return null;
 
     const dataName = match[0][match[0].length-1];
+    //console.log("first pass completed");
     return getPathData(path, dataName);
     //return firstDataArr;
 }
@@ -13,8 +14,10 @@ const getFirstData = (path) => {
 
 
 
-const getDataPoints_M_L = (path) => {
-    const regex = /(M|L)((\r\n|\r|\n)+| )?(\-?\+?)(\d+((\.)(\d+))?)((\r\n|\r|\n)+| |,)+(\-?\+?)(\d+((\.)(\d+))?)/i;
+const getDataPoints_M_L = (path, dataName) => {
+    const regexStr = `${dataName}((\\r\\n|\\r|\\n)+| )?(\\-?\\+?)(\\d+((\\.)(\\d+))?)((\\r\\n|\\r|\\n)+| |,)+(\\-?\\+?)(\\d+((\.)(\\d+))?)`;
+    const regex = new RegExp(regexStr, "i");
+    //const regex = /(M|L)((\r\n|\r|\n)+| )?(\-?\+?)(\d+((\.)(\d+))?)((\r\n|\r|\n)+| |,)+(\-?\+?)(\d+((\.)(\d+))?)/i;
     const _match = path.match(regex);
     if(!_match) return null;
 
@@ -25,6 +28,27 @@ const getDataPoints_M_L = (path) => {
 
     return obj;
 
+}
+
+const getDataPoints_Z = (path, dataName) => {
+    const _newPath = removeSegment(path, dataName);
+    const obj = {data: [dataName], newPath: _newPath}
+    return obj;
+}
+
+const getDataPoints_H_V = (path, dataName) => {
+    const regexStr = `${dataName}((\\r\\n|\\r|\\n)+| )?(\\-?\\+?)(\\d+((\\.)(\\d+))?)`;
+    const regex = new RegExp(regexStr, "i");
+    //const regex = /(M|L)((\r\n|\r|\n)+| )?(\-?\+?)(\d+((\.)(\d+))?)((\r\n|\r|\n)+| |,)+(\-?\+?)(\d+((\.)(\d+))?)/i;
+    const _match = path.match(regex);
+    if(!_match) return null;
+
+    console.log(_match[0]);
+    const _newPath = removeSegment(path, _match[0]);
+
+    const obj = {data: _match[0], newPath: _newPath}
+
+    return obj;
 }
 
 
@@ -61,18 +85,24 @@ const parseSegment = (segment) => {
 /**
  * finds the given data from the given path.
  * @param {string} path 
- * @param {string} data 
+ * @param {string} dataName 
  */
-const getPathData = (path, data) => {
-    const _data = data.toLowerCase();
+const getPathData = (path, dataName) => {
+    const _data = dataName.toLowerCase();
     
     switch (_data) {
         case "m":
-            return getDataPoints_M_L(path);
+            return getDataPoints_M_L(path, dataName);
             //break;
-        case "s":
-            
-            break;
+        case "l":
+            return getDataPoints_M_L(path, dataName);
+        case "h":
+            console.log("case h");
+            return getDataPoints_H_V(path, dataName);
+            //break;
+        case "v":
+            return getDataPoints_H_V(path, dataName);
+            //break;
         case "M":
             
             break;
@@ -88,15 +118,9 @@ const getPathData = (path, data) => {
         case "M":
             
             break;
-        case "M":
-            
-            break;
-        case "M":
-            
-            break;
-        case "M":
-            
-            break;  
+        case "z":
+            return getDataPoints_Z(path, dataName);
+            //break;  
     
         default:
             break;
